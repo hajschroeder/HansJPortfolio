3d1ed7160be4ed6bd9afc50d657aa5a9e2188ca1
Hans Schroeder <hajschroeder@gmail.com>
2023-05-14 00:30:30 -0700

    include image, further layout changes

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,16 +1,25 @@
 import React, { Fragment } from "react";
+import { Link as NavLink } from 'react-router-dom'
 import homeImage from '../../assets/kevinPortfolio.jpeg'
 import classes from './Home.module.css'
+import SubCard from "../UI/SubCard";
+import { Link } from "react-router-dom";
 
-const Home  = ()=> {
+const Home = () => {
 
   return (
     <Fragment>
 
-      <h1>Home Placeholder</h1>
+
+      <h3>Professional Full Stack Developer</h3>
       <div className={classes['main-image']}>
         <img src={homeImage} alt='literally me!' />
       </div>
+      <SubCard>
+        <NavLink className={classes.active} to='/about'>
+          About
+        </NavLink>
+      </SubCard>
 
     </Fragment>
   )
