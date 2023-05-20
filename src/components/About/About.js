5bf0dc4cee0c53b6de2c0534744dcef46c385e12
Hans Schroeder <hajschroeder@gmail.com>
2023-05-20 12:43:13 -0700

    updated resume

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,20 +2,20 @@ import React, { Fragment } from 'react';
 import classes from './About.module.css';
 import Card from '../UI/Card';
 import SubCard from '../UI/SubCard';
-import PdfResume from '../../assets/ResumeMay23.pdf'
+import Resume from '../../assets/Resume.pdf'
 import PdfModal from '../UI/PdfModal';
 import Techstack from './Techstack';
 
 
 
 const About = () => {
-  const resume = PdfResume
+  const resume = Resume
   return (
     <Fragment>
       <div className={classes.about}>
         <h2>About</h2>
         <Card>
-          <p>Hey! I'm Hans, and I'm a professional full-stack web and software developer based out of beautiful Portland, Oregon.<br /><br />Possessing a highly ecclectic background, I am able to take a unique and rich approach to writing code and collaborating on projects. With the creativity of a musician, the problem solving skills of a mechanic, and the organization of a Letter Carrier, I am able to bring a unique quality efficiency and perspective to all projects which I contribute</p>
+          <p>Hey! I'm Hans, and I'm a professional full-stack web and software developer based out of beautiful Portland, Oregon.<br /><br />Possessing a highly ecclectic background, I am able to take a unique and rich approach to writing code and collaborating on projects. With the creativity of a musician, the problem solving skills of a mechanic, and the organization of a Letter Carrier, I am able to provide a high quality efficiency and perspective to all projects which I contribute.</p>
           <PdfModal file={resume} />
         </Card>
       </div>
--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -65,7 +65,22 @@ const Techstack = () => {
         </div>
         <div className={classes.icons}>
           <Col >
+            <SiIcons.SiFirebase />
+          </Col>
+        </div>
+        <div className={classes.icons}>
+          <Col >
             <SiIcons.SiPostman />
+          </Col>
+        </div>
+        <div className={classes.icons}>
+          <Col >
+            <SiIcons.SiCsharp />
+          </Col>
+        </div>
+        <div className={classes.icons}>
+          <Col >
+            <SiIcons.SiDotnet />
           </Col>
         </div>
 
@@ -77,57 +92,3 @@ const Techstack = () => {
 export default Techstack;
 
 
-// import React from "react";
-// import { Col, Row } from "react-bootstrap";
-// import { CgCPlusPlus } from "react-icons/cg";
-// import {
-//   DiJavascript1,
-//   DiReact,
-//   DiNodejs,
-//   DiMongodb,
-//   DiPython,
-//   DiGit,
-// } from "react-icons/di";
-// import {
-//   SiPytorch,
-//   SiFirebase,
-//   SiNextdotjs,
-// } from "react-icons/si";
-// import classes from './About.module.css'
-// import Card from "../Layout/Card";
-
-// function Techstack() {
-//   return (
-//     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-//       <Col xs={4} md={2} className={classes["tech-icons"]}>
-//         <DiJavascript1 alt='javacript'/>
-//       </Col>
-//       <Col xs={4} md={2} className={classes["tech-icons"]}>
-//         <DiNodejs />
-//       </Col>
-//       <Col xs={4} md={2} className={classes["tech-icons"]}>
-//         <DiReact />
-//       </Col>
-//       <Col xs={4} md={2} className={classes["tech-icons"]}>
-//         <DiMongodb />
-//       </Col>
-//       <Col xs={4} md={2} className={classes["tech-icons"]}>
-//         <SiNextdotjs />
-//       </Col>
-//       <Col xs={4} md={2} className={classes["tech-icons"]}>
-//         <DiGit />
-//       </Col>
-//       <Col xs={4} md={2} className={classes["tech-icons"]}>
-//         <SiFirebase />
-//       </Col>
-//       <Col xs={4} md={2} className={classes["tech-icons"]}>
-//         <DiPython />
-//       </Col>
-//       <Col xs={4} md={2} className={classes["tech-icons"]}>
-//         <SiPytorch />
-//       </Col>
-//     </Row>
-//   );
-// }
-
-// export default Techstack;
