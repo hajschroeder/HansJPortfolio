d6ad8687fdf01ca7c696baced3dea7e43fc6b4f8
Hans Schroeder <hajschroeder@gmail.com>
2023-05-23 18:30:21 -0700

    styles

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,12 +3,13 @@ import { Link as NavLink } from 'react-router-dom'
 import homeImage from '../../assets/hjs.jpg'
 import classes from './Home.module.css'
 import SubCard from "../UI/SubCard";
-
+// import Card from "../UI/Card";
 const Home = () => {
 
   return (
     <Fragment>
       <div className={classes.home}>
+        {/* <Card> */}
         <h2>Full Stack Developer</h2>
         <p>Building and creating unique solutions. There is no problem so complex that cannot be solved with creativity.</p>
         <blockquote>
@@ -18,7 +19,7 @@ const Home = () => {
         <div className={classes['main-image']}>
           <img src={homeImage} alt='literally me!' />
         </div>
-
+        {/* </Card> */}
       </div>
       <SubCard>
         <NavLink className={classes.active} to='/about'>
